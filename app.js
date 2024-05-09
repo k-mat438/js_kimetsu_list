d82cf2936682ee0bd8fda665820719d1e8de963d
mat <a1sco00ocs1a@gmail.com>
2024-05-09 22:41:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 "use strict";
 
-// fetch('https://ihatov08.github.io/kimetsu_api/api/all.json').then(function(response) {
-//   return response.json()
-// }).then(function(json){
-//   console.log(json);
-//   for (const character of json) {
-//     console.log(character)
-//   }
-// })
+const loading = document.getElementById('loading');
+const container = document.getElementById('characters-container');
 
-async function fetchCharacters() {
+async function fetchCharacters(category) {
+  container.innerHTML = '';
+  loading.style.display = 'block';
+  // await new Promise(function(resolve) {
+  //   container.innerHTML = '';
+  //   loading.style.display = 'block';
+  //   resolve();
+  // })
+  await new Promise(resolve => setTimeout(resolve, 200));
   const response = await fetch('https://ihatov08.github.io/kimetsu_api/api/all.json');
   const json = await response.json();
+  // console.log(json);
   for (const character of json) {
-    // console.log(character)
     const card = document.createElement('div');
     card.classList.add('character-card');
-    card.innerHTML = `
-      <img src="https://ihatov08.github.io/${character.image}" alt="${character.name}" class="character-image">
-      <div class="character-name">"${character.name}"</div>
-      <ul class="character-category">
-          <li>"${character.category}"</li>
-      </ul>`;
-    console.log(card)
-    const container = document.querySelector('.characters-container');
-    container.appendChild(card);
-    console.log(container);
+    if (character.category == category || category == 'all') {
+      card.innerHTML = `
+        <img src="https://ihatov08.github.io/${character.image}" alt="${character.name}" class="character-image">
+        <div class="character-name">${character.name}</div>
+        <ul class="character-category">
+            <li>${character.category}</li>
+        </ul>`;
+      container.appendChild(card);
+    } 
   }
+  loading.style.display = 'none';
 }
 
-fetchCharacters();
 
-// const card = document.createElement('div');
-// card.classList.add('character-card');
-// card.innerHTML = `
-//   <img src="https://example.com/characters/tanjiro.jpg" alt="竈門炭治郎" class="character-image">
-//   <div class="character-name">竈門炭治郎</div>
-//   <ul class="character-category">
-//       <li>鬼殺隊</li>
-//   </ul>`;
-// console.log(card)
-// const container = document.querySelector('.characters-container');
-// container.appendChild(card);
-// console.log(container);
+function changeCategory() {
+  const radioButtons = document.querySelectorAll('input[name="category"]');
+  for (const radioButton of radioButtons) {
+    radioButton.addEventListener('change', function(event) {
+      fetchCharacters(event.target.value);
+    });
+  }  
+}
+
+fetchCharacters('all');
+changeCategory();
